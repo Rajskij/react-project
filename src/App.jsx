6854e132580b9712f406a73025e9ef406ce14ac3
Rajski <ihor.haidukov@gmail.com>
2025-07-07 21:08:27 -0400

    configured tailwind + shadcn lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from "./components/ui/theme-provider"
+import { ModeToggle } from "./components/ModeToggle"
+
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ModeToggle />
+        <h1>Vite + React</h1>
+      </ThemeProvider>
     </>
   )
 }
